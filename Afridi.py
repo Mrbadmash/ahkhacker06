6614ff1b26f1d64eca25d1a2e08163afd71a3c39
Mr Badmash <63794512+Mrbadmash@users.noreply.github.com>
2020-05-25 20:56:02 +0500

    Update Afridi.py

--- Afridi.py
+++ Afridi.py
@@ -37,13 +37,6 @@ def jalan(z):
 		time.sleep(00000.1)
 ##### LOGO #####
 logo = """
-   Asif Javed
-   ▄︻̷̿┻̿═━一
-   THE ANONYMOUS lovehacker TRICKER 
-   THE LEGEND ℒℴνℯ
-   THE GAME CHANGER ℒℴνℯ
-   pakistani hackers
-   ℒℴνℯ ▄︻̷̿┻̿═━一
 \033[1;91m=======================================
 \033[1;96mAuthor  \033[1;93m: \033[1;92mlove
 \033[1;96mYouTube \033[1;93m: \033[1;92mPakistani Hackers
